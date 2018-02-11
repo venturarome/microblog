479e0befbb1c4d45e46ce12eeefa780a6eba19aa
Ventura R Mendo <ventura.romendo@gmail.com>
2018-02-11 23:45:12 +0000

    Chapter 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,36 @@
+from flask import render_template
 from app import app    # from 'app' (package) import 'app' (variable)
 
-@app.route('/')
+#=== Following tutorial
 @app.route('/index/')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Ventura'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Index', user=user, posts=posts)
+
+
+# INFO from here still not working.
+
+
+#=== Home Page
+@app.route('/')
+@app.route('/home/')
+def home():
+    # TODO add links not hardcoding, but using url_for, as explained in:
+    # https://stackoverflow.com/questions/28207761/where-does-flask-look-for-image-files
+    return render_template('home.html')
+
+#=== About the author
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 

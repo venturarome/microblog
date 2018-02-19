b541703d5a976cd8420c99c6e4bf99a0afcaa466
Ventura R Mendo <ventura.romendo@gmail.com>
2018-02-19 21:16:12 +0000

    Chapter 11: Facelift

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 ############################################################
@@ -22,6 +23,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 ############################################################

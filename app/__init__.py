80eb37e8305ece100845668c8c9220edff2bf623
Ventura R Mendo <ventura.romendo@gmail.com>
2018-02-25 15:25:02 +0000

    Chapter 18: Deployment on Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -74,17 +74,24 @@ def create_app(config_class=Config):
             mail_handler.setLevel(logging.ERROR)  # CRITICAL > ERROR > WARNING > INFO > DEBUG > NOTSET
             app.logger.addHandler(mail_handler)
 
-        # Enable a file based log
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10) # Keep in storage a max of 10 logfiles, of a max of 10KB each.
-        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/microblog.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('MicroBlog startup')
-        
+        app.logger.info('Microblog startup')
+ 
     return app
 
 ############################################################
--- config.py
+++ config.py
@@ -6,11 +6,13 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     # Related to cryptographic signs (cookies and some extensions require this).
-    SECRET_KEY = os.environ.get('SECRET_KEY') #or '6w_pyHQhb4A-KL%bq7kZAq~sq.H7R/Ya'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '6w_pyHQhb4A-KL%bq7kZAq~sq.H7R/Ya'
     # Database config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Log system:
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     # Email config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER')                  # for gmail: smtp.goglemail.com
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)           # for gmail: 587
